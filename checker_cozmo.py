04d07e138376b34537e0c766e7593b2364b0279d
Abhinav Joshi <abhinavmjoshi@gmail.com>
2020-09-12 15:28:13 -0400

    added findMajority

--- checker_cozmo.py
+++ checker_cozmo.py
@@ -17,6 +17,7 @@ except ImportError:
 gyro = []
 acc = []
 
+
 class RobotStateDisplay(cozmo.annotate.Annotator):
     def apply(self, image, scale):
         d = ImageDraw.Draw(image)
@@ -24,7 +25,8 @@ class RobotStateDisplay(cozmo.annotate.Annotator):
         bounds = [3, 0, image.width, image.height]
 
         def print_line(text_line):
-            text = cozmo.annotate.ImageText(text_line, position=cozmo.annotate.TOP_LEFT, outline_color='black', color='lightblue')
+            text = cozmo.annotate.ImageText(
+                text_line, position=cozmo.annotate.TOP_LEFT, outline_color='black', color='lightblue')
             text.render(d, bounds)
             TEXT_HEIGHT = 11
             bounds[1] += TEXT_HEIGHT
@@ -35,7 +37,8 @@ class RobotStateDisplay(cozmo.annotate.Annotator):
 
         pose = robot.pose
         print_line('Pose: Pos = <%.1f, %.1f, %.1f>' % pose.position.x_y_z)
-        print_line('Pose: Rot quat = <%.1f, %.1f, %.1f, %.1f>' % pose.rotation.q0_q1_q2_q3)
+        print_line('Pose: Rot quat = <%.1f, %.1f, %.1f, %.1f>' %
+                   pose.rotation.q0_q1_q2_q3)
         print_line('Pose: angle_z = %.1f' % pose.rotation.angle_z.degrees)
         print_line('Pose: origin_id: %s' % pose.origin_id)
         gyro.append(robot.gyro.x_y_z)
@@ -48,80 +51,86 @@ class RobotStateDisplay(cozmo.annotate.Annotator):
         # Display the Accelerometer and Gyro data for the mobile device
 
         if robot.device_accel_raw is not None:
-            print_line('Device Acc Raw: <%.2f, %.2f, %.2f>' % robot.device_accel_raw.x_y_z)
+            print_line('Device Acc Raw: <%.2f, %.2f, %.2f>' %
+                       robot.device_accel_raw.x_y_z)
         if robot.device_accel_user is not None:
-            print_line('Device Acc User: <%.2f, %.2f, %.2f>' % robot.device_accel_user.x_y_z)
+            print_line('Device Acc User: <%.2f, %.2f, %.2f>' %
+                       robot.device_accel_user.x_y_z)
         if robot.device_gyro is not None:
             mat = robot.device_gyro.to_matrix()
             print_line('Device Gyro Up: <%.2f, %.2f, %.2f>' % mat.up_xyz)
             print_line('Device Gyro Fwd: <%.2f, %.2f, %.2f>' % mat.forward_xyz)
             print_line('Device Gyro Left: <%.2f, %.2f, %.2f>' % mat.left_xyz)
 
+
 def cozmo_program(robot: cozmo.robot.Robot):
-        print("Cozmo SDK has behavior control...")
-        robot.world.image_annotator.add_annotator('robotState', RobotStateDisplay)
-        robot.enable_device_imu(True, True, True)
-        robot.camera.image_stream_enabled = True
-        robot.camera.color_image_enabled = False
-        robot.camera.enable_auto_exposure()
-        robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
-        robot.move_lift(-1)
+    print("Cozmo SDK has behavior control...")
+    robot.world.image_annotator.add_annotator('robotState', RobotStateDisplay)
+    robot.enable_device_imu(True, True, True)
+    robot.camera.image_stream_enabled = True
+    robot.camera.color_image_enabled = False
+    robot.camera.enable_auto_exposure()
+    robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
+    robot.move_lift(-1)
 
-        robot.say_text("Hello World!").wait_for_completed()
-        time.sleep(1)
-        robot.say_text("Let's take pictures!").wait_for_completed()
-        time.sleep(1)
+    robot.say_text("Hello World!").wait_for_completed()
+    time.sleep(1)
+    robot.say_text("Let's take pictures!").wait_for_completed()
+    time.sleep(1)
 
-        # take pictures
-        # myargs = ["1", "order", "drone", "inspection"]
-        myargs = sys.argv[1:]
-        
-        if len(myargs) <= 1:
-            sys.exit("Incorrect arguments")
-        
-        num_images_per_type = int(myargs[0])  # number of images to take of each type of object
-        
-        print("Taking ", num_images_per_type, "images each of ", myargs[1:])
+    # take pictures
+    # myargs = ["1", "order", "drone", "inspection"]
+    myargs = sys.argv[1:]
 
-        for type in myargs[1:]:
-            for i in range(num_images_per_type):
-                time.sleep(.5)
+    if len(myargs) <= 1:
+        sys.exit("Incorrect arguments")
 
-                robot.say_text(type).wait_for_completed()
+    # number of images to take of each type of object
+    num_images_per_type = int(myargs[0])
 
-                time.sleep(3)
+    print("Taking ", num_images_per_type, "images each of ", myargs[1:])
 
-                latest_image = robot.world.latest_image
-                new_image = latest_image.raw_image
+    for type in myargs[1:]:
+        for i in range(num_images_per_type):
+            time.sleep(.5)
 
-                timestamp = datetime.datetime.now().strftime("%dT%H%M%S%f")
+            robot.say_text(type).wait_for_completed()
 
-                new_image.save("./outputs/" + str(type) + "_" + timestamp + ".bmp")
+            time.sleep(3)
 
-                time.sleep(3)
-        time.sleep(0.5)
-        robot.say_text("I will drive now!").wait_for_completed()
-        time.sleep(0.5)
-        robot.drive_straight(distance_mm(1000), speed_mmps(1000)).wait_for_completed()
-        robot.say_text("Check out my arms!").wait_for_completed()
-        robot.move_lift(1)
-        time.sleep(1)
-        robot.move_lift(-1)
-        time.sleep(1)
-        robot.say_text("Pick me up and shake me around!").wait_for_completed()
-        time.sleep(2)
-        robot.say_text("You can put me down now!").wait_for_completed()
-        f = open("./outputs/test_output.txt", 'r+')
-        f.seek(0)
-        f.write("Min Gyro: " + str(min(gyro)) + "\n")
-        f.write("Max Gyro: " + str(max(gyro)) + "\n")
-        f.write("Min Acc: " + str(min(acc)) + "\n")
-        f.write("Max Acc: " + str(max(acc)))
-        f.truncate()
-        f.close()
-        if (all(i > -10 for i in min(gyro)) and all(i < 10 for i in max(gyro))) and (all(i > -25000 for i in min(acc)) and all(i < 25000 for i in max(acc))):
-            robot.say_text("Sensors look good!").wait_for_completed()
-        else:
-            robot.say_text("Sensors look bad!").wait_for_completed()
+            latest_image = robot.world.latest_image
+            new_image = latest_image.raw_image
+
+            timestamp = datetime.datetime.now().strftime("%dT%H%M%S%f")
+
+            new_image.save("./outputs/" + str(type) + "_" + timestamp + ".bmp")
+
+            time.sleep(3)
+    time.sleep(0.5)
+    robot.say_text("I will drive now!").wait_for_completed()
+    time.sleep(0.5)
+    robot.drive_straight(distance_mm(
+        1000), speed_mmps(1000)).wait_for_completed()
+    robot.say_text("Check out my arms!").wait_for_completed()
+    robot.move_lift(1)
+    time.sleep(1)
+    robot.move_lift(-1)
+    time.sleep(1)
+    robot.say_text("Pick me up and shake me around!").wait_for_completed()
+    time.sleep(2)
+    robot.say_text("You can put me down now!").wait_for_completed()
+    f = open("./outputs/test_output.txt", 'r+')
+    f.seek(0)
+    f.write("Min Gyro: " + str(min(gyro)) + "\n")
+    f.write("Max Gyro: " + str(max(gyro)) + "\n")
+    f.write("Min Acc: " + str(min(acc)) + "\n")
+    f.write("Max Acc: " + str(max(acc)))
+    f.truncate()
+    f.close()
+    if (all(i > -10 for i in min(gyro)) and all(i < 10 for i in max(gyro))) and (all(i > -25000 for i in min(acc)) and all(i < 25000 for i in max(acc))):
+        robot.say_text("Sensors look good!").wait_for_completed()
+    else:
+        robot.say_text("Sensors look bad!").wait_for_completed()
+
 
 cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
--- fsm.py
+++ fsm.py
@@ -1,4 +1,6 @@
-import joblib, time, sklearn
+import joblib
+import time
+import sklearn
 import cozmo
 from cozmo.util import degrees
 import numpy as np
@@ -11,8 +13,6 @@ def wait_for_state(robot: cozmo.robot.Robot):
     robot.camera.color_image_enabled = False
     robot.camera.enable_auto_exposure()
     robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
-    robot.move_lift(0)
-    robot.say_text("I'm done").wait_for_completed()
 
     # Step 2: Process images and extract the state from the image using our image classification model
     image_classifier = joblib.load('trained_model.pkl')
@@ -29,13 +29,28 @@ def wait_for_state(robot: cozmo.robot.Robot):
             pred = image_classifier.predict(feats)
             predictions.append(pred)
             print(pred)
-        
-
 
         # Step 3: Encode the proper behavior for every state
 
 
+def find_majority(k):
+    myMap = {}
+    maximum = ('', 0)  # (occurring element, occurrences)
+    for n in k:
+        if n in myMap:
+            myMap[n] += 1
+        else:
+            myMap[n] = 1
+
+        # Keep track of maximum on the go
+        if myMap[n] > maximum[1]:
+            maximum = (n, myMap[n])
+
+    return maximum
+
 # Step 4: Write main method to run the wait_for_state function
+
+
 def main():
     cozmo.run_program(wait_for_state)
 
