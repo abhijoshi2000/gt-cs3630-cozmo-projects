3a44cd28ba349cf712cdc148059921aedc80a5a9
vab10266 <vaudalexburton@gmail.com>
2020-09-12 15:26:39 -0400

    Predictions made

--- fsm.py
+++ fsm.py
@@ -1,6 +1,8 @@
-import joblib
+import joblib, time, sklearn
 import cozmo
 from cozmo.util import degrees
+import numpy as np
+from imgclassification_sol import ImageClassifier
 
 
 def wait_for_state(robot: cozmo.robot.Robot):
@@ -9,18 +11,33 @@ def wait_for_state(robot: cozmo.robot.Robot):
     robot.camera.color_image_enabled = False
     robot.camera.enable_auto_exposure()
     robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
-    robot.move_lift(-1)
+    robot.move_lift(0)
+    robot.say_text("I'm done").wait_for_completed()
 
     # Step 2: Process images and extract the state from the image using our image classification model
+    image_classifier = joblib.load('trained_model.pkl')
     while True:
-        image_classifier = joblib.load('trained_model.pkl')
+        predictions = []
+        for i in range(10):
+            time.sleep(0.1)
+            latest_image = robot.world.latest_image
+            new_image = latest_image.raw_image
+
+            img = np.array(new_image)
+            img = np.expand_dims(img, axis=0)
+            feats = ImageClassifier.extract_image_features(0, img)
+            pred = image_classifier.predict(feats)
+            predictions.append(pred)
+            print(pred)
+        
+
 
         # Step 3: Encode the proper behavior for every state
 
 
 # Step 4: Write main method to run the wait_for_state function
 def main():
-    cozmo.run_program(wait_for_state).wait_for_completed()
+    cozmo.run_program(wait_for_state)
 
 
 if __name__ == '__main__':
