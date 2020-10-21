407e2104568d43370e720d7e36795720e85fba3e
Abhinav Joshi <abhinavmjoshi@gmail.com>
2020-10-21 14:56:24 -0400

    Abhi's portion of Lab 4

--- Lab 4/go_to_goal_cozmo.py
+++ Lab 4/go_to_goal_cozmo.py
@@ -1,5 +1,5 @@
-## If you run into an "[NSApplication _setup] unrecognized selector" problem on macOS,
-## try uncommenting the following snippet
+# If you run into an "[NSApplication _setup] unrecognized selector" problem on macOS,
+# try uncommenting the following snippet
 
 # try:
 #     import matplotlib
@@ -26,7 +26,9 @@ from setting import *
 from particle_filter import *
 from utils import *
 
-#particle filter functionality
+# particle filter functionality
+
+
 class ParticleFilter:
 
     def __init__(self, grid):
@@ -39,25 +41,28 @@ class ParticleFilter:
         self.particles = motion_update(self.particles, odom)
 
         # ---------- Sensor (markers) model update ----------
-        self.particles = measurement_update(self.particles, r_marker_list, self.grid)
+        self.particles = measurement_update(
+            self.particles, r_marker_list, self.grid)
 
         # ---------- Show current state ----------
         # Try to find current best estimate for display
         m_x, m_y, m_h, m_confident = compute_mean_pose(self.particles)
         return (m_x, m_y, m_h, m_confident)
 
+
 # tmp cache
-last_pose = cozmo.util.Pose(0,0,0,angle_z=cozmo.util.Angle(degrees=0))
+last_pose = cozmo.util.Pose(0, 0, 0, angle_z=cozmo.util.Angle(degrees=0))
 flag_odom_init = False
 
 # goal location for the robot to drive to, (x, y, theta)
-goal = (6,10,0)
+goal = (6, 10, 0)
 
 # map
 Map_filename = "map_arena.json"
 grid = CozGrid(Map_filename)
 gui = GUIWindow(grid, show_camera=True)
 pf = ParticleFilter(grid)
+
 
 def compute_odometry(curr_pose, cvt_inch=True):
     '''
@@ -75,7 +80,7 @@ def compute_odometry(curr_pose, cvt_inch=True):
         last_pose.rotation.angle_z.degrees
     curr_x, curr_y, curr_h = curr_pose.position.x, curr_pose.position.y, \
         curr_pose.rotation.angle_z.degrees
-    
+
     dx, dy = rotate_point(curr_x-last_x, curr_y-last_y, -last_h)
     if cvt_inch:
         dx, dy = dx / grid.scale, dy / grid.scale
@@ -108,21 +113,24 @@ async def marker_processing(robot, camera_settings, show_diagnostic_image=False)
     # Convert the image to grayscale
     image = np.array(image_event.image)
     image = color.rgb2gray(image)
-    
+
     # Detect the markers
-    markers, diag = detect.detect_markers(image, camera_settings, include_diagnostics=True)
+    markers, diag = detect.detect_markers(
+        image, camera_settings, include_diagnostics=True)
 
     # Measured marker list for the particle filter, scaled by the grid scale
     marker_list = [marker['xyh'] for marker in markers]
-    marker_list = [(x/grid.scale, y/grid.scale, h) for x,y,h in marker_list]
+    marker_list = [(x/grid.scale, y/grid.scale, h) for x, y, h in marker_list]
 
     # Annotate the camera image with the markers
     if not show_diagnostic_image:
-        annotated_image = image_event.image.resize((image.shape[1] * 2, image.shape[0] * 2))
+        annotated_image = image_event.image.resize(
+            (image.shape[1] * 2, image.shape[0] * 2))
         annotator.annotate_markers(annotated_image, markers, scale=2)
     else:
         diag_image = color.gray2rgb(diag['filtered_image'])
-        diag_image = Image.fromarray(np.uint8(diag_image * 255)).resize((image.shape[1] * 2, image.shape[0] * 2))
+        diag_image = Image.fromarray(
+            np.uint8(diag_image * 255)).resize((image.shape[1] * 2, image.shape[0] * 2))
         annotator.annotate_markers(diag_image, markers, scale=2)
         annotated_image = diag_image
 
@@ -145,23 +153,61 @@ async def run(robot: cozmo.robot.Robot):
     cx, cy = robot.camera.config.center.x_y
     camera_settings = np.array([
         [fx,  0, cx],
-        [ 0, fy, cy],
-        [ 0,  0,  1]
+        [0, fy, cy],
+        [0,  0,  1]
     ], dtype=np.float)
-            
-    ###################
-
-    # YOUR CODE HERE
 
     ###################
+
+    reached_goal = False
+    has_converged = False
+
+    while True:
+        # Two broad cases - if the robot is not localized, and if it is localized
+        if not has_converged:
+            # Check if the robot is picked up - if so, relocalize
+            if robot.is_picked_up:
+                await robot.say_text("I've been kidnapped!").wait_for_completed()
+                pf = ParticleFilter(grid)
+                continue
+            # Else try to converge
+            marker_list, annotated_image = await marker_processing(robot, camera_settings)
+            x, y, z, has_converged = pf.update(
+                compute_odometry(robot.pose), marker_list)
+            update_gui(x, y, z, has_converged, annotated_image)
+            last_pose = robot.pose
+            if has_converged:
+                await robot.drive_wheels(-10.00, 2.00)
+        else:
+            # If the robot is localized - has_converged = True
+            update_gui(x, y, z, has_converged, annotated_image)
+            # If the robot is at the goal state and then is picked up
+            if reached_goal:
+                # FILL IN YOUR CODE HERE
+                continue
+            # If the robot is not at the goal state and then is picked up
+            else:
+                # FILL IN YOUR CODE HERE
+                continue
+
+    ###################
+
+
+def update_gui(x, y, z, has_converged, annotated_image):
+    gui.show_particles(pf.particles)
+    gui.show_mean(x, y, z, has_converged)
+    gui.show_camera_image(annotated_image)
+    gui.updated.set()
+
 
 class CozmoThread(threading.Thread):
-    
+
     def __init__(self):
         threading.Thread.__init__(self, daemon=False)
 
     def run(self):
-        cozmo.robot.Robot.drive_off_charger_on_connect = False  # Cozmo can stay on his charger
+        # Cozmo can stay on his charger
+        cozmo.robot.Robot.drive_off_charger_on_connect = False
         cozmo.run_program(run, use_viewer=False)
 
 
@@ -175,4 +221,3 @@ if __name__ == '__main__':
     gui.show_particles(pf.particles)
     gui.show_mean(0, 0, 0)
     gui.start()
-
